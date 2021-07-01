1e2c74104f68ea597fb0b71e4dbf87e2d9a5150f
henry hyeonwoo hwang <daintree.henry@gmail.com>
2021-07-02 00:43:21 +0900

    change version

--- tododbsingle/docker_run.sh
+++ tododbsingle/docker_run.sh
@@ -1,1 +1,1 @@
-docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=tododb -d -p 3306:3306 dainforest/tododbsingle
+docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=tododb -d -p 3306:3306 dainforest/tododbsingle:1.0
